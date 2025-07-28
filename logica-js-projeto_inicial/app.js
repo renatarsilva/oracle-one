81bd195ffb1fe83f18a7fbcd5833a326923154a0
Renata <renata.rodriguesdasilva@outlook.com>
2025-07-28 15:32:45 -0300

    feat update

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,53 +1,24 @@
-// // Challenges
+alert("Boas vindas ao jogo do número secreto");
 
-// // alert("Boas vindas ao nosso site!");
+let numeroSecreto = 5;
+console.log(numeroSecreto);
 
-// // let name = "Lua";
+let chute;
+let tentativas = 0;
 
-// // let idade1 = 25;
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número entre 1 e 10");
 
-// // let saldoDisponivel = 1000;
-
-// // alert("Erro! Preencha todos os campos!");
-
-// // let mensagemDeErro = "Erro! Preencha todos os campos!";
-// // alert(mensagemDeErro);
-
-// // let nome = prompt("Digite seu nome:");
-
-// // let idade = 18;
-
-// // let idade2 = prompt("Digite sua idade");
-
-// // if (idade2 >= idade) {
-// //   alert("Voce já pode tirar a habilitação!");
-// // } else {
-// //   alert("Voce ainda nao pode tirar a habilitação");
-// // }
-
-// // 2
-// // let numeroSecreto = 8;
-
-// // let chute = prompt("Digite um número");
-
-// // if (numeroSecreto == chute) {
-// //   alert(`Isso ai! Voce descobriu o ${numeroSecreto}`);
-// // } else {
-// //   alert("Voce nao descobriu o :( ");
-// // }
-
-// alert("Bem-vindo ao jogo do número secreto");
-
-// let chute = prompt("Escolha um número entre 1 e 10");
-
-// let numeroSecreto = 4;
-
-// console.log(chute == numeroSecreto);
-// if (chute == numeroSecreto) {
-//   alert("Acertou");
-// } else {
-//   alert(
-//     "O número secreto era 55                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        " +
-//       numeroSecreto
-//   );
-// }
+  if (chute == numeroSecreto) {
+    alert(
+      `Parabéns, você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`
+    );
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+  }
+}
