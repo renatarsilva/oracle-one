5e6c6780dbecd8e9a141703d418b0fa800eee5c3
Renata <renata.rodriguesdasilva@outlook.com>
2025-07-28 12:45:30 -0300

    feat: desafios

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,53 @@
-alert("Hello World");
+// // Challenges
+
+// // alert("Boas vindas ao nosso site!");
+
+// // let name = "Lua";
+
+// // let idade1 = 25;
+
+// // let saldoDisponivel = 1000;
+
+// // alert("Erro! Preencha todos os campos!");
+
+// // let mensagemDeErro = "Erro! Preencha todos os campos!";
+// // alert(mensagemDeErro);
+
+// // let nome = prompt("Digite seu nome:");
+
+// // let idade = 18;
+
+// // let idade2 = prompt("Digite sua idade");
+
+// // if (idade2 >= idade) {
+// //   alert("Voce já pode tirar a habilitação!");
+// // } else {
+// //   alert("Voce ainda nao pode tirar a habilitação");
+// // }
+
+// // 2
+// // let numeroSecreto = 8;
+
+// // let chute = prompt("Digite um número");
+
+// // if (numeroSecreto == chute) {
+// //   alert(`Isso ai! Voce descobriu o ${numeroSecreto}`);
+// // } else {
+// //   alert("Voce nao descobriu o :( ");
+// // }
+
+// alert("Bem-vindo ao jogo do número secreto");
+
+// let chute = prompt("Escolha um número entre 1 e 10");
+
+// let numeroSecreto = 4;
+
+// console.log(chute == numeroSecreto);
+// if (chute == numeroSecreto) {
+//   alert("Acertou");
+// } else {
+//   alert(
+//     "O número secreto era 55                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        " +
+//       numeroSecreto
+//   );
+// }
---
+++ logica-js-projeto_inicial/desafio.js
@@ -0,0 +1,36 @@
+// 1
+// alert("Olá");
+
+// let diaSemana = prompt("Qual é o dia da semana");
+
+// if (diaSemana == "Sábado" || diaSemana == "Domingo") {
+//   alert("Bom fim de semana");
+// } else {
+//   alert("Boa semana!");
+// }
+
+// 2
+// let numero = prompt("Digite um número");
+
+// if (numero % 2 == 0) {
+//   alert("O número é positivo!");
+// } else {
+//   alert("O número é negativo! ");
+// }
+
+// 3
+// let suaPontuacao = prompt("Digite a sua pontuação: ");
+
+// if (suaPontuacao >= 100) {
+//   alert("Parabéns voce venceu!");
+// } else {
+//   alert("Tente novamente");
+// }
+
+//4
+// let saldo = prompt("Digite seu saldo: ");
+// alert(`Seu saldo é ${saldo}`);
+
+//5
+// let nome = prompt("Digite seu nome: ");
+// alert(`Seja bem vindo ${nome}`);
