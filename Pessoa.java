d93b67b4f6b1250364235ebe5854483e4c50a532
Renata <renata.rodriguesdasilva@outlook.com>
2025-11-20 15:55:21 -0300

    feat update

--- Pessoa.java
+++ Pessoa.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+public class Pessoa{
+    public String nome;
+    public String cpf;
+    public String email;
 
-public class Pessoa {
+    public String exibirDados(){
+        return "nome: " + this.nome + "\n"+
+                "CPF: " + this.cpf + "\n" +
+                "email: "+ this.email +"\n";
+    }
+
+    public static void main(String[] args) {
+        Pessoa andre = new Pessoa();
+        andre.nome = "André da silva";
+        andre.cpf = "123456-78";
+        andre.email = "andre@email";
+        System.out.println(andre.exibirDados());
+    }
+
+//    public static void main(String[] args) {
+//        Pessoa maki = new Pessoa();
+//        maki.nome = "Maki Zenin";
+//        maki.cpf = "987654-32";
+//        maki.email = "maki@email.com";
+//        System.out.println(maki.exibirDados());
+//    }
 }
