a7321de140a7411128a428c34cb2f534534fc59b
Renata <renata.rodriguesdasilva@outlook.com>
2025-11-20 17:34:23 -0300

    feta: update ex2 Temperatura

---
+++ primeiroProgramaEmJava/Temperatura.java
@@ -0,0 +1,9 @@
+package primeiroProgramaEmJava;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        double temperaturaCelsius = 36.5;
+        double conversaoFahrenheit = (temperaturaCelsius * 1.8) + 32;
+        System.out.println("A temperatura em Fahrenheit é : " + conversaoFahrenheit);
+    }
+}
