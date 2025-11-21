d4a77cd1865f57c449e58c2bc5110e1fe6e723df
Renata <renata.rodriguesdasilva@outlook.com>
2025-11-20 22:29:26 -0300

    feat; update ex: while

---
+++ primeiroProgramaEmJava/While.java
@@ -0,0 +1,25 @@
+package primeiroProgramaEmJava;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int totalNotas = 0;
+
+        while (nota != -1) {
+            System.out.println("Diga sua avaliacao para o filme ou -1 para encerrar");
+            nota = leitura.nextDouble();
+
+            if (nota != -1) {
+
+            mediaAvaliacao += nota;
+            totalNotas++;
+            }
+        }
+        System.out.println("Média de avaliacoes " + mediaAvaliacao/totalNotas);
+    }
+}
